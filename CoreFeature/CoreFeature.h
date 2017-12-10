4a3cb6983cc01da35e9959df8e3084581c133380
Ron Daryl Magno <ron.zohan@gmail.com>
2017-12-11 00:17:54 +0800

    Initial

---
+++ CoreFeature/CoreFeature.h
@@ -0,0 +1,19 @@
+//
+//  CoreFeature.h
+//  CoreFeature
+//
+//  Created by Ron Daryl Magno on 12/11/17.
+//  Copyright © 2017 Ron Daryl Magno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreFeature.
+FOUNDATION_EXPORT double CoreFeatureVersionNumber;
+
+//! Project version string for CoreFeature.
+FOUNDATION_EXPORT const unsigned char CoreFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreFeature/PublicHeader.h>
+
+
